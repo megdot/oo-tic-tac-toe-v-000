317ea62e752badf71d7aef23b8de663b9855708d
Megan Dotto <megan.dotto@gmail.com>
2018-03-12 22:35:41 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,18 +2,16 @@ class TicTacToe
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
   end
-
-  WIN_COMBINATIONS = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [6, 4, 2]
+  WIN_COMBINATIONS =[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [0,4,8]
   ]
-
   def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
@@ -21,78 +19,89 @@ class TicTacToe
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
-
-  def move(location, player = "X")
-    location = location.to_i
-    @board[location - 1] = player
+  
+  def input_to_index(user_input)
+    user_input.to_i - 1
   end
-
-  def position_taken?(location)
-    !(@board[location].nil? || @board[location] == " ")
+  
+ def move(index,current_player)
+   @board[index] = current_player
+ end
+ 
+ def position_taken?(location)
+   @board[location] != " " && @board[location] != ""
+ end
+ 
+ def valid_move?(index)
+   index.between?(0,8) && !position_taken?(index) ? true:false
+ end
+ 
+ def turn
+   puts "Please enter 1-9:"
+   input = gets.strip
+   index = input_to_index(input)
+   if valid_move?(index)
+      move(index, current_player)
+      display_board
+   else
+    turn
   end
-
-  def valid_move?(location)
-    location = location.to_i
-    location.between?(1,9) && !position_taken?(location - 1)
-  end
-
-  def turn
-    puts "Please enter a number between 1 and 9: "
-    input = gets.to_i
-    if !valid_move?(input)
-      turn
-    end
-    move(input, current_player)
-    display_board
-  end
-
-  def turn_count
-    @board.count{|token| token == "X" || token == "O"}
-  end
-
-  def current_player
-    turn_count % 2 == 0 ? "X" : "O"
-  end
-
-  def won?
-    WIN_COMBINATIONS.detect do |win_combo|
-      position_taken?(win_combo[0]) && (@board[win_combo[0]] == @board[win_combo[1]]) && (@board[win_combo[1]] == @board[win_combo[2]])
-    end
-  end
-
-  def full?
-    WIN_COMBINATIONS.all? do |tokens|
-      position_taken?(tokens[0])
-    end
-  end
-
-  def draw?
-    full? && !won?
-  end
-
-  def over?
-    won? || draw?
-  end
-
-  def winner
-    WIN_COMBINATIONS.detect do |winner|
-      if position_taken?(winner[0]) && (@board[winner[0]] == "X") && (@board[winner[1]] == "X") && (@board[winner[2]] == "X")
-        return "X"
-      elsif position_taken?(winner[0]) && (@board[winner[0]] == "O") && (@board[winner[1]] == "O") && (@board[winner[2]] == "O")
-        return "O"
+ end 
+ 
+ def turn_count
+     counter=0
+   @board.each do |turn|
+     if turn !=" "
+     counter += 1
+   end
+ end
+   return counter
+ end
+ def current_player
+   turn_count.even? ? "X":"O"
+ end
+ def won?
+     WIN_COMBINATIONS.detect do |win_combination|
+  win_index_1 = win_combination[0]
+  win_index_2 = win_combination[1]
+  win_index_3 = win_combination[2]
+ 
+  position_1 = @board[win_index_1]
+  position_2 = @board[win_index_2]
+  position_3 = @board[win_index_3]
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"
+       return win_combination
+      end
+      if position_1 == "O" && position_2 == "O" && position_3 == "O"
+     return   win_combination
       end
     end
+  else
+    false
   end
-
+  def full?
+   @board.include?(" ") ? false:true
+    end
+  def draw?
+    !won? && full? ? true:false
+  end
+  def over?
+    won? || full? || draw? ? true:false
+  end
+  def winner
+    if win_player = won?
+    @board[win_player[0]]
+   end
+  end
   def play
-    until over?
-      turn
-    end
-
-    if won?
-      puts "Congratulations #{winner}!"
-    elsif draw?
-      puts "Cats Game!"
-    end
+    while !over?
+    turn
   end
+  if won?
+    puts"Congratulations #{winner}!"
+  elsif draw?
+    puts "Cat's Game!"
+  end
+  end
+  
 end
